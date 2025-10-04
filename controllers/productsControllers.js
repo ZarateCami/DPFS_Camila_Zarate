8e97fbeb75bbe866f0c556878bd00f0ed1738faf
ZarateCami <camilazarate9427@gmail.com>
2025-10-03 23:27:00 -0300

    add products/create

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -5,19 +5,29 @@ let productPath = path.join(__dirname, "../data/products.json")
 console.log(productPath)
 
 let productController = {
-  createProduct: function (req, res) {
+  formCreate: function (req, res) {
     res.render("products/create")
   },
-  
 
-  newProduct: function (req, res) {
-    console.log(req.body)
+  createProduct: function (req, res) {
+    const product = JSON.parse(fs.readFileSync(productPath, "utf-8"))
+    let newProduct = {
+      id: product.length + 1,
+      name: req.body.name,
+      description: req.body.description,
+      image: req.file?.filename || "not-found.jpg",
+      category: req.body.category,
+      colors: req.body.colors,
+      price: req.body.price,
+    }
+    product.push(newProduct)
+    fs.writeFileSync(productPath, JSON.stringify(product, null, " "))
     res.redirect("/products/list")
   },
 
   editProduct: function (req, res) {
     console.log(req.params.id)
-    res.render("products/edit")
+    res.render("/products/edit")
   },
 
   productList: function (req, res) {
@@ -35,9 +45,9 @@ let productController = {
   productDetail: function (req, res) {
     let productsJson = fs.readFileSync(productPath, "utf-8") /*Lee el JSON*/
     let products = JSON.parse(productsJson) /*Lo convierte en JS*/
-    let prod = products.find(product => product.id == req.params.id)
-    res.render("products/productDetail", {prod})
-  }
+    let prod = products.find((product) => product.id == req.params.id)
+    res.render("products/productDetail", { prod })
+  },
 }
 
 module.exports = productController
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -4,10 +4,11 @@ var router = express.Router()
 
 /* GET products page. */
 /*GET sirve para "Obtener/Leer" información del servidor*/
-router.get("/create", productController.createProduct)
+
+router.get("/create", productController.formCreate)
 
 /*POST sirve para "Enviar/Crear" información al servidor*/
-router.post("/create", productController.newProduct)
+router.post("/create", productController.createProduct)
 
 router.get("/edit/:id", productController.editProduct)
 /*El método para recibir la info del form sería put*/
